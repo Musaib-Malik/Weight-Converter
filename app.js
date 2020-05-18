f2c319a225df5fabdadc1b235aae94c4125fb12b
Musaib <atomicsight809@gmail.com>
2020-05-18 23:55:26 +0530

    Gave the finishing touch

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 // UI Variables
-const weight = document.querySelector('.weight');
-const weightList = document.getElementById('weightList');
-let converted = document.querySelector('.converted');
-const form = document.querySelector('form');
+const weight = document.querySelector(".weight");
+const weightList = document.getElementById("weightList");
+let converted = document.querySelector(".converted");
+const form = document.querySelector("form");
 
 // Event Handler
-form.addEventListener('submit', convertValue);
+form.addEventListener("submit", convertValue);
 
 // Submit Event
 function convertValue(e) {
   e.preventDefault();
 
-  if (weightList.value == 'K') {
-    converted.value = `${parseFloat(weight.value * 2.205).toFixed(1)} (Lbs)`
-  } else if (weightList.value == 'L') {
-    converted.value = `${parseFloat(weight.value / 2.205).toFixed(1)} (Kilo)`;
+  if (weightList.value == "K") {
+    converted.value = `${parseFloat(weight.value * 2.205).toFixed(1)} (Lbs)`;
+  } else if (weightList.value == "L") {
+    converted.value = `${parseFloat(weight.value / 2.205).toFixed(1)} (Kg)`;
   }
 }
